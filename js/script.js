9a4337431f71c612f6feeb055641155e31b0a53e
Maria Jamille <maria.jamille1984@gmail.com>
2023-10-09 16:41:11 -0300

    Mudança no modal da imagem

--- js/script.js
+++ js/script.js
@@ -14,51 +14,9 @@
 const modal01 = document.querySelector(".dia01")
 const modal02 = document.querySelector(".dia02")
 const modal03 = document.querySelector(".dia03")
-const closeAll01 = document.querySelector("#close-all01")
-const closeAll02 = document.querySelector("#close-all02")
-const closeAll03 = document.querySelector("#close-all03")
 const containerModal = document.querySelector("#modal-js")
-// var btn = document.querySelector("#close1")
-// var container = document.querySelector("#modal-teste")
-// var abrir = document.querySelector("#abrir")
-// var close = document.querySelector("#close")
 
 
-// btn.onclick = function(){
-//     modal.style.display = 'none'
-//     // container.style.display = 'none'
-// }
-
-// btn.addEventListener('click', function () {
-//     // if (modal.style.display === 'block') {
-//         modal.style.display = 'none'
-//     // } else {
-//     //     btn.style.display = 'none'
-//     //     container.style.display = 'block'
-//     // }
-// })
-
-// btn.addEventListener('click', function () {
-
-// if (container.style.display === 'block') {
-// container.style.display = 'none'
-// modal.style.display = 'none'
-// } else {
-//     btn.style.display = 'none'
-//     container.style.display = 'block'
-// }
-// })
-
-// close.onclick = () => {
-//     container.style.display = 'none'
-//     btn.style.display = 'block'
-// }
-
-// const exit_btn = info_box.querySelector(".buttons .quit")
-// const continue_btn = info_box.querySelector(".buttons .restart")
-// const info_box = document.querySelector(".info_box")
-
-// const teste = document.querySelector(".modal")
 const button = document.querySelector("button")
 const buttonClose01 = document.querySelector("#close01")
 const buttonClose02 = document.querySelector("#close02")
@@ -70,49 +28,6 @@ const wordText = document.querySelector(".word"),
     refreshBtn = document.querySelector(".refresh-word"),
     checkBtn = document.querySelector("#check-word"),
     confirmar = document.querySelector("#mostrarDiv");
-// var img = document.getElementById('imagem')
-
-// button.onclick = function () {
-//     modal01.show()
-// }
-button.onclick = function () {
-    modal01.show()
-    modal02.show()
-    modal03.show()
-}
-
-// const start_btn = document.querySelector(".start_btn button");
-// const container = document.querySelector(".container");
-
-// if startQuiz button clicked
-// start_btn.onclick = ()=>{
-//     container.classList.add("activeInfo"); //show info box
-// }
-
-// if exitQuiz button clicked
-// exit_btn.onclick = ()=>{
-//     container.classList.remove("activeInfo"); //hide info box
-// }
-
-
-// let correctWord, timer;
-
-// const initTimer = maxTime => {
-//     clearInterval(timer);
-//     timer = setInterval(() => {
-//         if(maxTime > 0) {
-//             maxTime--;
-//             return timeText.innerText = maxTime;
-//         }
-//         alert(`Opa! Seu tempo acabou! ${correctWord.toUpperCase()} é a palavra correta!`);
-//         initGame();
-//     }, 1000);
-// }
-
-// continue_btn.onclick = () => {
-//     info_box.classList.remove("activeInfo"); //hide info box
-//     quiz_box.classList.add("activeQuiz"); //show quiz box
-// }
 
 const initGame = () => {
     // initTimer(30);
@@ -133,9 +48,9 @@ initGame();
 
 const checkWord = () => {
     let userWord = inputField.value.toLowerCase();
-    if (!userWord) return modal01.show();
-    if (userWord !== correctWord) return modal03.show();
-    modal02.show();
+    if (!userWord) return modal01.show(), divTransparente.style.display = 'block';
+    if (userWord !== correctWord) return modal03.show(), divTransparente.style.display = 'block';
+    modal02.show() , d2.style.display = 'block';
     initGame();
 }
 refreshBtn.addEventListener("click", initGame);
@@ -190,53 +105,21 @@ buttonClose03.onclick = () => {
 const mostrarDivButton = document.getElementById('check-word');
 const divTransparente = document.getElementById('divTransparente');
 
-mostrarDivButton.addEventListener('click', () => {
-    divTransparente.style.display = 'block';
-});
+// mostrarDivButton.addEventListener('click', () => {
+//     divTransparente.style.display = 'block';
+// });
 
 divTransparente.addEventListener('click', () => {
     divTransparente.style.display = 'none';
     modal01.close()
-    modal02.close() 
-    modal03.close() 
+    modal03.close()
+    // Adicione aqui o código da ação que deseja executar quando a div transparente for clicada.
+});
+d2.addEventListener('click', () => {
+    d2.style.display = 'none';
+    modal02.close()
     // Adicione aqui o código da ação que deseja executar quando a div transparente for clicada.
 });
 
-// divTransparente.onclick = function () {
-//     // modal02.close()  
-//     // divTransparente.style.display = 'none';
-//     document.querySelector(".exampleModalToggle2").remove()
-//     modal02.style.display = 'none';
-// }
-
-
-// divTransparente.addEventListener('click', function () {
-
-//     if (container.style.display === 'block') {
-//         container.style.display = 'none'
-//     } else {
-//         btn.style.display = 'none'
-//         container.style.display = 'block'
-//     }
-// })
-
-
-// Butões acionados com teclas:
-// document.addEventListener("keypress", function(o){
-//     if(o.key === 'Enter'){
-//         checkBtn.click()
-//     }
-// });
-
-
-
-// confirmar.addEventListener("keypress", function(e) {
-
-//     if(e.key === "Enter"){
-
-//        confirmar.click();
-
-//     }
-// });
 
 
